a55538611a569ad25314749066ce8680ffa7f52b
SergeevYaroslav <sergeevyaroslav91@gmail.com>
2024-03-28 18:54:26 +0200

    Initial commit

---
+++ src/Lesson2Task1.java
@@ -0,0 +1,13 @@
+public class Lesson2Task1 {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 9;
+        byte z = (byte) b;
+        byte x = (byte) a;
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(b);
+        System.out.println(z);
+
+    }
+}
